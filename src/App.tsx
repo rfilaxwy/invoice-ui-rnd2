b7c2cc011297041630422e8e433bf2bdf111a4ce
Ryan Filax-Wylie <ryanfilaxwylie@gmail.com>
2020-03-09 05:56:54 -0600

    Worked out bug in assigning backend to state in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { Component } from 'react';
 import Layout from './components/Layout/Layout';
 import Invoice from './components/Invoice/Invoice';
 import CompanyHome from './components/CompanyHome/CompanyHome';
@@ -13,27 +13,28 @@ export type inv = {
 }
 
 
-const App = () => {
-
-  const [invoices, setInvoices] = useState(
-    [
-      {
-        invoiceNumber: 'null',
-        lines: [
-          {
-            "id": 0,
-            "service": 'n/a',
-            "cost": 0,
-            "quantity": 0,
-            "units": 'n/a',
-            "description": 'n/a'
-          }
-        ]
-      }
+export default class App extends Component {
+  state = {
+    invoices: [{
+      invoiceNumber: 'null',
+      lines: [
+        {
+          "id": 0,
+          "service": 'n/a',
+          "cost": 0,
+          "quantity": 0,
+          "units": 'n/a',
+          "description": 'n/a'
+        }
+      ]
+    }
     ]
-  );
+  }
 
-  const getInvoiceLines = async () => {
+
+
+
+  getInvoiceLines = async () => {
     const response = await fetch(`/api/invoice/`);
     const body = await response.json();
     if (response.status !== 200) {
@@ -42,66 +43,70 @@ const App = () => {
     return body;
   };
 
-  useEffect(() => {
-    getInvoiceLines()
+  componentDidMount() {
+    let currentInvoices = this.state;
+    this.getInvoiceLines()
       .then(res => {
-        let invLines = res.data;
-        console.log(invLines)
-        if (invLines) setInvoices([...invLines]);
+        let invoices = res;
+        console.log(res)
+        let newInvoices = Object.assign({}, invoices);
+        console.log(newInvoices)
+        if (invoices) this.setState({ invoices: invoices });
 
       })
       .catch(err => { console.log(`Error ${err}`) })
-  })
-  let lineLinks
-  if (invoices) {
-    lineLinks = invoices.map((inv, index) => {
-      let path = `/invoice/${inv.invoiceNumber}`;
-      return (
-        <Route key={index} path={path}>
-          <Layout>
-            <Invoice
-              invoiceNumber={parseInt(inv.invoiceNumber)}
-            />
-          </Layout>
-        </Route>
-      )
-    })
   }
-  return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/invoice"> New Invoice</Link>
-            </li>
-          </ul>
-        </nav>
 
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/invoice">
+  render() {
+    let lineLinks
+    if (this.state.invoices) {
+      lineLinks = this.state.invoices.map((inv, index) => {
+        let path = `/invoice/${inv.invoiceNumber}`;
+        return (
+          <Route key={index} path={path}>
             <Layout>
               <Invoice
-                invoiceNumber={3}
+                invoiceNumber={parseInt(inv.invoiceNumber)}
               />
             </Layout>
           </Route>
-          {lineLinks}
-          <Route exact path="/">
-            <CompanyHome
-              invoices={invoices}
-            />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+        )
+      })
+    }
+    return (
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/invoice"> New Invoice</Link>
+              </li>
+            </ul>
+          </nav>
 
-  );
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/invoice">
+              <Layout>
+                <Invoice
+                  invoiceNumber={3}
+                />
+              </Layout>
+            </Route>
+            {lineLinks}
+            <Route exact path="/">
+              <CompanyHome
+                invoices={this.state.invoices}
+              />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+
+    );
+  }
 }
-
-export default App;
